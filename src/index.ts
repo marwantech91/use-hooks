70c75df5bdc7f4f8e06a10cb6b63b505bfbffcac
Marwan Saleh <marwan.saleh.tech@gmail.com>
2026-03-12 10:36:24 +0300

    Add useClickOutside hook for dropdown/modal handling

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 export { useLocalStorage } from './useLocalStorage';
 export { useDebounce, useDebouncedCallback } from './useDebounce';
 export { useClickOutside } from './useClickOutside';
+
+// useClickOutside - Detect clicks outside an element
+export function useClickOutside<T extends HTMLElement>(
+  callback: () => void
+): React.RefObject<T> {
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClick);
+    return () => document.removeEventListener('mousedown', handleClick);
+  }, [callback]);
+
+  return ref;
+}
