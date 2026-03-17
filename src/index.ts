6a2df4d3e42db642f0e1600ef79ae76211cece79
Marwan Saleh <marwan.saleh.tech@gmail.com>
2026-03-17 16:46:18 +0300

    Remove duplicate useClickOutside implementation

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,3 @@ export { useLocalStorage } from './useLocalStorage';
 export { useDebounce, useDebouncedCallback } from './useDebounce';
 export { useClickOutside } from './useClickOutside';
 export { useMediaQuery } from './useMediaQuery';
-
-// useClickOutside - Detect clicks outside an element
-export function useClickOutside<T extends HTMLElement>(
-  callback: () => void
-): React.RefObject<T> {
-  const ref = useRef<T>(null);
-
-  useEffect(() => {
-    const handleClick = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        callback();
-      }
-    };
-
-    document.addEventListener('mousedown', handleClick);
-    return () => document.removeEventListener('mousedown', handleClick);
-  }, [callback]);
-
-  return ref;
-}
